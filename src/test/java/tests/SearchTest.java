4d899c2868021fedcd416b2eac8b2187e83f1aa0
pavelstepanovv <stepanovpasa38@gmail.com>
2026-07-14 07:02:10 +0300

    Исправлен SearchTest

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -6,21 +6,33 @@ import pages.SearchResultPage;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * Тест №1: Поиск товара по ключевому слову.
+ * Проверяет, что поиск работает и выдаёт товары с нужным словом в названии.
+ */
 public class SearchTest extends BaseTest {
+    // СТАТИЧЕСКИЕ ПОЛЯ
+    private static final String SEARCH_QUERY = "наушники";
 
     @Test
     public void testSearchProduct() {
+        // 1. Открыть главную страницу
         MainPage mainPage = new MainPage().open();
-        SearchResultPage results = mainPage.search("наушники");
 
+        // 2. Выполнить поиск
+        SearchResultPage results = mainPage.search(SEARCH_QUERY);
+
+        // 3. Проверить, что товары найдены
         assertThat(results.hasProducts())
-                .as("Должны быть найдены товары по запросу 'наушники'")
+                .as("Должны быть найдены товары по запросу '%s'", SEARCH_QUERY)
                 .isTrue();
 
-        assertThat(results.hasProductContainingWord("наушники"))
-                .as("Хотя бы один товар должен содержать слово 'наушники' в названии")
+        // 4. Проверить, что есть товар со словом "наушники" в названии
+        assertThat(results.hasProductContainingWord(SEARCH_QUERY))
+                .as("Хотя бы один товар должен содержать слово '%s' в названии", SEARCH_QUERY)
                 .isTrue();
 
+        // 5. Вывод информации для отладки
         System.out.println("Найдено товаров: " + results.getProductsCount());
         System.out.println("Первый товар: " + results.getFirstProductTitle() + " | Цена: " + results.getFirstProductPrice());
     }
