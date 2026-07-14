392d5f118d7b838c471d6165aef4f834a494f17d
pavelstepanovv <stepanovpasa38@gmail.com>
2026-07-14 06:57:19 +0300

    Исправлен класс Input

--- src/test/java/elements/Input.java
+++ src/test/java/elements/Input.java
@@ -1,28 +1,55 @@
 package elements;
 
+/**
+ * Класс-наследник от BaseElement для работы с полями ввода на странице.
+ */
 public class Input extends BaseElement {
 
+    // СТАТИЧЕСКИЕ ПОЛЯ (локаторы)
     private static final String INPUT_BY_NAME_XPATH = "//input[@name='%s']";
     private static final String INPUT_BY_ID_XPATH = "//input[@id='%s']";
 
+    // КОНСТРУКТОРЫ
     private Input(String xpath, String value) {
         super(xpath, value);
     }
 
-    public void fill(String text) {
-        element.clear();
-        element.sendKeys(text);
-    }
-
+    // ПУБЛИЧНЫЕ МЕТОДЫ
+    /**
+     * Создаёт объект поля ввода по атрибуту name.
+     *
+     * Параметры: name значение атрибута name
+     * Возвращает: объект Input
+     */
     public static Input byName(String name) {
         return new Input(INPUT_BY_NAME_XPATH, name);
     }
 
+    /**
+     * Создаёт объект поля ввода по атрибуту id.
+     *
+     * Параметры: id значение атрибута id
+     * Возвращает: объект Input
+     */
     public static Input byId(String id) {
         return new Input(INPUT_BY_ID_XPATH, id);
     }
 
+    /**
+     * Заполняет поле ввода текстом.
+     * Очищает поле, затем вводит текст.
+     *
+     * Параметры: text текст, который нужно ввести
+     */
+    public void fill(String text) {
+        clear();
+        sendKeys(text);
+    }
+
+    /**
+     * Очищает поле ввода.
+     */
     public void clear() {
-        element.clear();
+        super.clear();
     }
 }
