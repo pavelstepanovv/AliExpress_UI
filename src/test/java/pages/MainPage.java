6f056d50d9601b025fadcb009791e4457ff3e77e
pavelstepanovv <stepanovpasa38@gmail.com>
2026-07-14 06:59:00 +0300

    Изменен MainPage

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,71 +1,41 @@
 package pages;
 
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
 import elements.Button;
 import elements.Input;
 
-import static com.codeborne.selenide.Selenide.$$x;
-
+/**
+ * Главная страница AliExpress, где начинаются все сценарии поиска товаров.
+ */
 public class MainPage extends BasePage {
 
+    // СТАТИЧЕСКИЕ ПОЛЯ
     private static final String URL = "https://aliexpress.ru";
 
+    // ПОЛЯ ЭКЗЕМПЛЯРА
     private final Input searchInput = Input.byName("SearchText");
     private final Button searchButton = Button.byText("Найти");
 
-    // Элементы для работы с подсказками
-    private final ElementsCollection suggestions = $$x("//div[contains(@class, 'suggestion')]");
-
+    // ПУБЛИЧНЫЕ МЕТОДЫ
+    /**
+     * Открывает главную страницу AliExpress.
+     *
+     * Возвращает: объект MainPage
+     */
     public MainPage open() {
         open(URL);
         return this;
     }
 
+    /**
+     * Выполняет поиск товара по ключевому слову.
+     * Вводит запрос и нажимает кнопку "Найти".
+     *
+     * Параметры: query поисковый запрос
+     * Возвращает: объект SearchResultPage (страница результатов)
+     */
     public SearchResultPage search(String query) {
         searchInput.fill(query);
         searchButton.click();
         return new SearchResultPage();
-    }
-
-    public void clearSearch() {
-        searchInput.clear();
-    }
-
-    public void fillSearch(String query) {
-        searchInput.fill(query);
-    }
-
-    public void fillSearchAndWaitSuggestions(String query) {
-        searchInput.fill(query);
-        // Ожидание появления подсказок (реализовано через Selenide)
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public int getSuggestionsCount() {
-        return suggestions.size();
-    }
-
-    public boolean allSuggestionsContain(String word) {
-        if (suggestions.isEmpty()) {
-            return false;
-        }
-        for (SelenideElement suggestion : suggestions) {
-            if (!suggestion.getText().toLowerCase().contains(word.toLowerCase())) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public String getFirstSuggestionText() {
-        if (suggestions.isEmpty()) {
-            return "Нет подсказок";
-        }
-        return suggestions.first().getText();
     }
 }
