cda896fd9626eb0b85c103eb3012bba8de01de87
pavelstepanovv <stepanovpasa38@gmail.com>
2026-07-14 06:55:48 +0300

    Исправлен класс Button

--- src/test/java/elements/Button.java
+++ src/test/java/elements/Button.java
@@ -1,24 +1,44 @@
 package elements;
 
+/**
+ * Класс-наследник от BaseElement для работы с кнопками на странице.
+ */
 public class Button extends BaseElement {
 
+    // СТАТИЧЕСКИЕ ПОЛЯ (локаторы)
     private static final String BUTTON_BY_TEXT_XPATH = "//button[contains(text(), '%s')]";
     private static final String BUTTON_BY_CLASS_XPATH = "//button[contains(@class, '%s')]";
-    private static final String BUTTON_BY_TYPE_XPATH = "//button[@type='%s']";
 
+    // КОНСТРУКТОРЫ
     private Button(String xpath, String value) {
         super(xpath, value);
     }
 
+    // ПУБЛИЧНЫЕ МЕТОДЫ
+    /**
+     * Создаёт объект кнопки по тексту на ней.
+     *
+     * Параметры: text текст кнопки
+     * Возвращает: объект Button
+     */
     public static Button byText(String text) {
         return new Button(BUTTON_BY_TEXT_XPATH, text);
     }
 
+    /**
+     * Создаёт объект кнопки по CSS-классу.
+     *
+     * Параметры: className название класса
+     * Возвращает: объект Button
+     */
     public static Button byClass(String className) {
         return new Button(BUTTON_BY_CLASS_XPATH, className);
     }
 
-    public static Button byType(String type) {
-        return new Button(BUTTON_BY_TYPE_XPATH, type);
+    /**
+     * Кликает по кнопке.
+     */
+    public void click() {
+        element.click();
     }
 }
